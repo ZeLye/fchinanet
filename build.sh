f2b527b55af1fa9961958e1bb37cc14c9a439228
01sr <chenbing8914@outlook.com>
2018-04-04 23:57:24 +0800

    接口更新

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 echo "start build in linux";
-CGO_ENABLED=0 GOOS=linux  go build -o share/fchinanet_linux/fchinanet fchinanet.go;
+CGO_ENABLED=0 GOOS=linux  go build -o build/fchinanet_linux/fchinanet fchinanet.go;
 echo "complete build in linux";
 echo "start build in windows";
-CGO_ENABLED=0 GOOS=windows go build -o share/fchinanet_windows/fchinanet.exe fchinanet.go;
+CGO_ENABLED=0 GOOS=windows go build -o build/fchinanet_windows/fchinanet.exe fchinanet.go;
 echo "complete build in windows";
 echo "start build in macos";
-CGO_ENABLED=0 GOOS=darwin go build -o share/fchinanet_mac/fchinanet fchinanet.go;
+CGO_ENABLED=0 GOOS=darwin go build -o build/fchinanet_mac/fchinanet fchinanet.go;
 echo "complete build in macos";
